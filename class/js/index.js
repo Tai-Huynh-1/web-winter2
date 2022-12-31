68afcbbd24004b621a27d7c7c7a3ada10689c6c8
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2022-12-30 21:46:26 -0800

    hoisting, var, functions and call stack

--- class/js/index.js
+++ class/js/index.js
@@ -21,8 +21,73 @@ let age2 = "50"; // string
 // console.log(1 == true);
 // console.log(0 == false);
 
-// RETURN TO THIS
-// console.log(Boolean(null));
-console.log(null == false);
-console.log(undefined == false);
-console.log(undefined == null);
+// RETURN TO THIS - abstract equality comparison algorithm
+// console.log(null == false); // false
+// console.log(undefined == false); // false
+// console.log(undefined == null); // true if null or undefined is on one side then null or undefined has to be on other side to resolve to true
+// console.log(null == undefined); // true
+// console.log(null == null); // true
+
+// let & var can be reassigned, const cannot be reassigned
+// var can be declared more than once (with same variable name), let & const cannot be declared more than once
+
+// accessing 'var' before initialization/declaration results in 'undefined' (HOISTING : seems like our variable (var) is brought to the top of the code and executed first, but it does not have a value assigned to it, JS assigns 'undefined')
+// var & functions get hoisted
+// 2 phases to executing JS: memory allocation & execution
+// var nameA;
+// console.log(nameA);
+var nameA = "Dave";
+// console.log(nameA);
+
+// let & const does not get hoisted; need to declare/init variable first before using; otherwise program throws an error
+let nameB = "Jack";
+// console.log(nameB);
+
+// // functions getting hoisted: ONLY works for functions declared using the 'function' keyword without storing inside of a variable let & const
+// add(5, 10);
+// function add(a, b) {
+// 	console.log(a + b);
+// }
+
+// // anonymous functions getting stored in a variable
+// // subtract(15, 8);
+// const subtract = function (a, b) {
+// 	console.log(a - b);
+// };
+
+// // arrow functions getting stored in a variable
+// multi(5, 4);
+// const multi = (a, b) => {
+// 	console.log(a * b);
+// };
+
+// control flow of JS program
+console.log("begin");
+
+function add(a, b) {
+	console.log("line 68", a + b);
+	return a + b;
+}
+
+console.log("line 72", add(8, 7));
+
+console.log("done");
+
+// begin > line 68 15 > line 69: 15 or error
+// begin > line 68: 15 > line 72: 15 > done || begin > undefined > done
+// begin > line 69: 15 > line 72: 15 > done
+// begin > done
+// queue: first in first out (FIFO)
+// stack: last in first out (stack files)
+
+// [] >
+// [index.js] >
+// [index.js + line 65 console.log] >
+// [index.js + line 72 console.log ] >
+// [index.js + line 72 console.log + add()] >
+// [index.js + line 72 console.log + add() + line 68 console.log] >
+// [index.js + line 72 console.log + add()] >
+// [index.js + line 72 console.log] >
+// [index.js + line 74 console.log] >
+// [index.js] >
+// []
